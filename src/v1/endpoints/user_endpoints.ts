cc7a8d071960155e081f1f953db533239b54239e
Jeree-Boiii <jerometer465@gmail.com>
2024-02-02 15:57:46 +0000

    Changed routes to "/api/v1/..." instead of just "/..."

--- src/v1/endpoints/user_endpoints.ts
+++ src/v1/endpoints/user_endpoints.ts
@@ -8,7 +8,7 @@ import * as User from "../db_handling/user";
 // Export module
 module.exports = function(app: Express, db: Db) {
     // POST: /users
-    // Create user
+    // Create a new user
     // Parameters:
     // --> userName: string
     // --> realName: string
@@ -19,17 +19,17 @@ module.exports = function(app: Express, db: Db) {
     // Returns
     // --> id: string (valid ObjectId)   | ID of inserted user
     // --> status: number                | HTTP Status Code
-    app.post("/users", async (req, res) => {
+    app.post("/api/v1/users", async (req, res) => {
         // Get + parse parameters
         let rawParams = req.query.params?.toString();
-        if (rawParams == undefined) {
+        if (!rawParams) {
             res.status(StatusCodes.NOT_ACCEPTABLE).send("ERROR: Parameters not provided");
             return;
         }
 
-        let {userName, realName, email, password, year, classLetter} = JSON.parse(rawParams);
+        let { userName, realName, email, password, year, classLetter } = JSON.parse(rawParams);
 
-        // Add user to database
+        // Add new user to database
         let response = await User.createUser(db, userName, realName, email, password, false, year, classLetter);
         res.status(response.status).send(JSON.stringify(response));
         return;
@@ -46,28 +46,53 @@ module.exports = function(app: Express, db: Db) {
     // Returns:
     // --> user: {}                          | Information about user
     // --> status: number                    | HTTP Status Code
-    app.get("/users", async (req, res) => {
+    app.get("/api/v1/users", async (req, res) => {
         // Get + parse token
         let rawToken = req.query.token?.toString();
-        if (rawToken == undefined) {
+        if (!rawToken) {
             res.status(StatusCodes.UNAUTHORIZED).send("ERROR: Token not provided");
             return;
         }
 
-        let {token, userId} = JSON.parse(rawToken);
+        let { token, userId } = JSON.parse(rawToken);
 
         // Get + parse parameters
         let rawParams = req.query.params?.toString();
-        if (rawParams == undefined) {
+        if (!rawParams) {
             res.status(StatusCodes.NOT_ACCEPTABLE).send("ERROR: Parameters not provided");
             return;
         }
 
-        let {targetId} = JSON.parse(rawParams);
+        let { targetId } = JSON.parse(rawParams);
 
         // Return user information
         let response = await User.getUserInfo(db, token, userId, targetId);
         res.status(response.status).send(JSON.stringify(response));
+        return;
+    });
+
+
+    // DELETE: /users
+    // Delete user
+    // REQUIRES TOKEN
+    // --> token: string (valid ObjectId)
+    // --> userId: string (valid ObjectId)
+    // Parameters:
+    // --> N/A
+    app.delete("/api/v1/users", async (req, res) => {
+        // Get + parse token
+        let rawToken = req.query.token?.toString();
+        if (!rawToken) {
+            res.status(StatusCodes.UNAUTHORIZED).send("ERROR: Token not provided");
+            return;
+        }
+
+        let { token, userId } = JSON.parse(rawToken);
+
+        // Return response
+        let response = await User.deleteUser(db, token, userId);
+        res.status(response.status).send(JSON.stringify(response));
+        return;
     });
 
 
@@ -77,19 +102,20 @@ module.exports = function(app: Express, db: Db) {
     // --> userName: string|null
     // --> email: string|null
     // --> password: string
-    app.post("/users/login", async (req, res) => {
+    app.post("/api/v1/users/login", async (req, res) => {
         // Get + parse parameters
         let rawParams = req.query.params?.toString();
-        if (rawParams == undefined) {
+        if (!rawParams) {
             res.status(StatusCodes.NOT_ACCEPTABLE).send("ERROR: Parameters not provided");
             return;
         }
 
-        let {userName, email, password} = JSON.parse(rawParams);
+        let { userName, email, password } = JSON.parse(rawParams);
 
         // Return login token
         let response = await User.login(db, userName, email, password);
         res.status(response.status).send(JSON.stringify(response));
+        return;
     });
 
 
@@ -100,41 +126,19 @@ module.exports = function(app: Express, db: Db) {
     // --> userId: string (valid ObjectId)
     // Parameters:
     // --> N/A
-    app.post("/users/logout", async (req, res) => {
+    app.post("/api/v1/users/logout", async (req, res) => {
         // Get + parse token
         let rawToken = req.query.token?.toString();
-        if (rawToken == undefined) {
+        if (!rawToken) {
             res.status(StatusCodes.UNAUTHORIZED).send("ERROR: Token not provided");
             return;
         }
 
-        let {token, userId} = JSON.parse(rawToken);
+        let { token, userId } = JSON.parse(rawToken);
 
         // Return response
         let response = await User.logout(db, token, userId);
         res.status(response.status).send(JSON.stringify(response));
-    });
-
-
-    // POST: /users/delete
-    // Delete user
-    // REQUIRES TOKEN
-    // --> token: string (valid ObjectId)
-    // --> userId: string (valid ObjectId)
-    // Parameters:
-    // --> N/A
-    app.post("users/delete", async (req, res) => {
-        // Get + parse token
-        let rawToken = req.query.token?.toString();
-        if (rawToken == undefined) {
-            res.status(StatusCodes.UNAUTHORIZED).send("ERROR: Token not provided");
-            return;
-        }
-
-        let {token, userId} = JSON.parse(rawToken);
-
-        // Return response
-        let response = await User.deleteUser(db, token, userId);
-        res.status(response.status).send(JSON.stringify(response));
+        return;
     });
 }
